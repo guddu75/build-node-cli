c75d0fe5fbf672ff315876feb567d2def11103bf
Debapriya Majumder <guddumajumder75@gmail.com>
2021-12-13 21:10:42 +0530

    Install dependencies for new CLI

--- utils/generate.js
+++ utils/generate.js
@@ -1,8 +1,11 @@
+const execa = require('execa');
+const ora = require('ora');
 const path = require('path');
 const copy = require('copy-template-dir');
 const alert = require('terminal-alerts');
-const { green : g , dim : d } = require('chalk');
+const { green : g , dim : d ,yellow : y} = require('chalk');
 
+const spinner = ora({text:``});
 const questions = require('./questions');
 
 module.exports = async () => {
@@ -11,7 +14,7 @@ module.exports = async () => {
     const inDirPath = path.join(__dirname,`../templates`);
     const outDirPath = path.join(process.cwd(), outDir );
 
-    copy(inDirPath,outDirPath,vars, (err, createdfiles) =>{
+    copy(inDirPath,outDirPath,vars, async (err, createdfiles) =>{
         if(err) throw err;
 
         console.log(d(`Creating fils in ${g(`./${outDir}`)} directory\n`));
@@ -19,6 +22,20 @@ module.exports = async () => {
         createdfiles.forEach(file => {
             console.log(`${g(`Created`)} ${file}`);
         });
+        
+        console.log();
+        spinner.start(`${y(`DEPENDENCIES`)} installing... \n\n ${d(`It may take a moment`)}`);
+        process.chdir(outDirPath);
+        const pkgs = [
+            `meow@9.0.0`,
+            `cli-meow-help`,
+            `terminal-alerts`,
+            `cli-handle-unhandled`,
+            `cli-welcome`
+        ];
+        await execa(`npm`,[`install`,...pkgs]);
+        await execa(`npm`,[`dedupe`]);
+        spinner.succeed(`${g(`DEPENDENCIES`)} installed`);
 
         alert({
             type : `success`,
